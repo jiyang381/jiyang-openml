e0f4b73ee482237d7d8cf2ed53fd4e6ba2619816
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2022-07-26 08:42:26 +0000

    aio: fix possible invalid memory access when DEBUG is enabled

--- openml-api/src/main/java/com/feedzai/openml/data/Dataset.java
+++ openml-api/src/main/java/com/feedzai/openml/data/Dataset.java
@@ -12,3 +12,27 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.data;
+
+import com.feedzai.openml.data.schema.DatasetSchema;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * A dataset is a container of instances that have the same data schema.
+ *
+ * @author Pedro Rijo (pedro.rijo@feedzai.com)
+ * @since 0.1.0
+ */
+public interface Dataset {
+
+    /**
+     * Gets the {@link DatasetSchema} associated with the data held by this Dataset.
+     *
+     * @return The {@link DatasetSchema}.
