5beaf85dbdc9538694283d38de7f4001c7cfb632
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2022-10-01 08:39:54 +0000

    Removed @namespace tags

--- openml-api/src/main/java/com/feedzai/openml/data/schema/NumericValueSchema.java
+++ openml-api/src/main/java/com/feedzai/openml/data/schema/NumericValueSchema.java
@@ -10,4 +10,27 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governin
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.data.schema;
+
+/**
+ * Represents the type of a feature whose values are numeric (all numeric types up to {@link java.lang.Double}).
+ *
+ * @author Pedro Rijo (pedro.rijo@feedzai.com)
+ * @since 0.1.0
+ */
+public class NumericValueSchema extends AbstractValueSchema {
+
+    /**
+     * Serial version UID.
+     */
+    private static final long serialVersionUID = -4471978224024450702L;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param allowMissing 
