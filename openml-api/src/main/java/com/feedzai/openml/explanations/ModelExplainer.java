39c01dbbe6a2e4403deaf01b8b5faf0f7d4b558d
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2022-10-02 12:38:25 +0000

    Delete README.rst

---
+++ openml-api/src/main/java/com/feedzai/openml/explanations/ModelExplainer.java
@@ -0,0 +1,41 @@
+
+/*
+ * Copyright 2022 Feedzai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.explanations;
+
+import com.feedzai.openml.data.Instance;
+import com.feedzai.openml.model.MachineLearningModel;
+
+/**
+ * An interface for explanation algorithms.
+ * Implementations should probably hold some {@link MachineLearningModel} and this {@link ModelExplainer} will
+ * compute the feature contributions for its predictions through {@link #getFeatureContributions(Instance)}.
+ *
+ * @since 1.2.0
+ * @author Miguel Lobo (miguel.lobo@feedzai.com)
+ */
+public interface ModelExplainer {
+
+    /**
+     * Gets the feature contribution scores for a particular {@link Instance}.
+     *
+     * @param instance the {@link Instance}.
+     * @return An array of feature contribution scores.
+     */
+    double[] getFeatureContributions(final Instance instance);
+}
--- openml-api/src/main/java/com/feedzai/openml/model/ClassificationMLModel.java
+++ openml-api/src/main/java/com/feedzai/openml/model/ClassificationMLModel.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2018 Feedzai
  *
@@ -11,14 +10,4 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-/**
- * Concepts about the schema (types) of the data.
- *
- * @since 0.1.0
- */
-package com.feedzai.openml.data.schema;
+ 
