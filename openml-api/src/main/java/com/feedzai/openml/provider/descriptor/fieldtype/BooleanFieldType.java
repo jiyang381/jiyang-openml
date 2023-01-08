aca71735af69e7472d8a0a8ca155032f89507f61
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-01-08 21:48:11 +0000

    Add calendar

--- openml-api/src/main/java/com/feedzai/openml/provider/descriptor/fieldtype/BooleanFieldType.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/descriptor/fieldtype/BooleanFieldType.java
@@ -17,4 +17,21 @@
 
 package com.feedzai.openml.provider.descriptor.fieldtype;
 
-import com.google.common.base.
+import com.google.common.base.MoreObjects;
+
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * A concrete implementation of a {@link ModelParameterType} for parameters whose values are boolean.
+ * Check {@link #validate(String, String)} documentation for valid boolean value representations.
+ *
+ * @author Pedro Rijo (pedro.rijo@feedzai.com)
+ * @since 0.1.0
+ */
+public class BooleanFieldType implements ModelParameterType {
+
+    /**
+     * Defines whether this field defaults to the value true.
+     */
+    private fi
