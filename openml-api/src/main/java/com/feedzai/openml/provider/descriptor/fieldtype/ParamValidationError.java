4871c63ae91600d7be6be0203caba85344dc0f82
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-01-12 21:33:47 +0000

    Remove unused files

--- openml-api/src/main/java/com/feedzai/openml/provider/descriptor/fieldtype/ParamValidationError.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/descriptor/fieldtype/ParamValidationError.java
@@ -7,4 +7,20 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unle
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.provider.descriptor.fieldtype;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+import java.util.Objects;
+
+/**
+ * A POJO representing a parame
