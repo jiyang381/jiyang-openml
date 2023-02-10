06e03aad16a902e4e545fa5eeb8d9a06196f2ff7
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-02-10 18:11:09 +0000

    StyleCop

--- openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelLoadingException.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelLoadingException.java
@@ -49,4 +49,15 @@ public class ModelLoadingException extends Exception {
      * this exception's detail message.
      *
      * @param message the detail message (which is saved for later retrieval
-     *             
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     */
+    public ModelLoadingException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and
