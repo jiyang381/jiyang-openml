8e659b82a0b89f8f43af72b042db1df0694119fc
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-02-13 17:24:45 +0000

    Updated how feature data is pushed;

--- openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
@@ -29,3 +29,19 @@ public class ModelTrainingException extends Exception {
      * The serial version UID.
      */
     private static final long serialVersionUID = 8216378348700228491L;
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public ModelTrainingException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p
