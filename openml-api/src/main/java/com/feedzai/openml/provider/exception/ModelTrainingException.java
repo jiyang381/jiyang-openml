89212b007a66bf0f692c88042eead1c13f3c9288
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-02-13 16:43:11 +0000

    Change port

--- openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
@@ -9,4 +9,23 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.provider.exception;
+
+/**
+ * Exception for errors occurred during the training of a ML Model.
+ *
+ * @author Pedro Rijo (pedro.rijo@feedzai.com)
+ * @since 0.1.0
+ */
+public class ModelTrainingException extends Exception {
+
+    /**
+     * The serial version UID.
+     */
+    private static final long serialVersionUID = 8216378348700228491L;
