b3de7e367c1df526b8912361e4b7c0d6485e9207
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-02-16 11:04:18 +0000

    added a comment

--- openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
+++ openml-api/src/main/java/com/feedzai/openml/provider/exception/ModelTrainingException.java
@@ -53,4 +53,16 @@ public class ModelTrainingException extends Exception {
      * @param cause   the cause (which is saved for later retrieval by the
      *                {@link #getCause()} method).  (A <tt>null</tt> value is
      *                permitted, and indicates that the cause is nonexistent or
-     * 
+     *                unknown.)
+     */
+    public ModelTrainingException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * java.security.PrivilegedActionExceptio
