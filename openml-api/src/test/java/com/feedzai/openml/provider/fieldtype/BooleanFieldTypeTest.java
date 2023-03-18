1be18f4bb89496b47efa2b4a6beea86f3a8f4a3f
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-03-18 16:11:49 +0000

    Updating Greek

--- openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/BooleanFieldTypeTest.java
+++ openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/BooleanFieldTypeTest.java
@@ -9,4 +9,26 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impli
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.provider.fieldtype;
+
+import com.feedzai.openml.provider.descriptor.fieldtype.BooleanFieldType;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests the behaviour of the {@link BooleanFieldType} class.
+ *
+ * @author Pedro Rijo (pedro.rijo@feedzai.com)
+ * @since 0.1.0
+ */
+public class BooleanFieldTypeTest extends AbstractConfigFieldTypeTest<BooleanFieldType> {
+
+    @Override
+    BooleanFieldType g
