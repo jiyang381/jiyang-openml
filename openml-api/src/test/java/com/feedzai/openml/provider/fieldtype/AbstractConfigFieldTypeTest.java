21257f1a05fb63fc3004a49926b69183e4e534dd
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-03-18 15:27:43 +0000

    Add app.json

--- openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/AbstractConfigFieldTypeTest.java
+++ openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/AbstractConfigFieldTypeTest.java
@@ -34,3 +34,82 @@ import static org.junit.Assert.assertEquals;
  *
  * @author Pedro Rijo (pedro.rijo@feedzai.com)
  * @since 0.1.0
+ */
+abstract class AbstractConfigFieldTypeTest<T extends ModelParameterType> {
+
+    /**
+     * Tests the string representation.
+     */
+    @Test
+    public void testToString() {
+        assertThat(getInstance().toString())
+                .isNotNull()
+                .isNotEmpty();
+    }
+
+    /**
+     * Tests the equals check.
+     */
+    @Test
+    public void testEquals() {
+        final T instance = getInstance();
+        assertThat(instance)
+                .isEqualTo(instance)
+                .isEqualTo(getInstance())
+                .isNotEqualTo(null)
+                .isNotEqualTo(getAnotherInstance());
+    }
+
+    /**
+     * Tests the hashed representation.
+     */
+    @Test
+    public void testHashCode() {
+        final T instance = getInstance();
+        assertThat(instance.hashCode())
+                .isEqualTo(instance.hashCode())
+                .isEqualTo(getInstance().hashCode())
+                .isNotEqualTo(getAnotherInstance().hashCode());
+    }
+
+    /**
+     * Gets an instance to test.
+     *
+     * @return An instance.
+     */
+    abstract T getInstance();
+
+    /**
+     * Gets an instance that is different from {@link #getInstance()}.
+     *
+     * @return A different instance in terms of properties.
+     */
+    abstract T getAnotherInstance();
+
+    /**
+     * Calls the {@link ModelParameterType#validate(String, String)} method with the given parameter name
+     * and value, and asserts if the return type contains an error or not,
+     * as expected through the fucntion parameter {@code expectsError}.
+     *
+     * @param fieldType The concrete implementation of {@link ModelParameterType} to test.
+     * @param paramName The name of the parameter.
+     * @param paramValue The value of the parameter, as a String.
+     * @param expectsError If the validation should return an error.
+     */
+    void assertValidationResult(final ModelParameterType fieldType,
+                                final String paramName,
+                                final String paramValue,
+                                final boolean expectsError) {
+
+        final Optional<ParamValidationError> result = fieldType.validate(paramName, paramValue);
+
+        assertEquals(
+                String.format("Param %s with value %s should %s, error: %s",
+                              paramName,
+                              paramValue,
+                              expectsError ? "not be valid" : "be valid",
+                              result.map(ParamValidationError::getMessage).orElse("")),
+                result.isPresent(),
+                expectsError);
+    }
+}
