667509b7105be1581e7327f0976b925a45e4e3d3
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-03-27 12:15:05 +0000

    Added StyleCI badge

--- openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/NumericFieldTypeTest.java
+++ openml-api/src/test/java/com/feedzai/openml/provider/fieldtype/NumericFieldTypeTest.java
@@ -7,4 +7,22 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, soft
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.provider.fieldtype;
+
+import com.feedzai.openml.provider.descriptor.fieldtype.NumericFieldType;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Tests
