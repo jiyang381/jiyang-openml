b1b002be5a39ff98653da18479bc9bd71ba1f82f
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-05-11 16:06:38 +0000

    updated build system

--- openml-example/src/test/java/com/feedzai/openml/example/ExampleModelLoaderTest.java
+++ openml-example/src/test/java/com/feedzai/openml/example/ExampleModelLoaderTest.java
@@ -7,4 +7,17 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.example;
+
+import com.feedzai.openml.data.schema.DatasetSchema;
+import com.feedzai.openml.provider.exception.ModelLoadingException;
+import com.feedzai.openml.util.data.schema.TestDatasetSchemaBuilder;
+import com.google.common.collect.
