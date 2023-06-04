f7744ef197f2aefde2432b4829f17e91f706918b
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-06-04 17:22:56 +0000

    Added readme.txt

--- openml-utils/src/main/java/com/feedzai/openml/mocks/MockInstance.java
+++ openml-utils/src/main/java/com/feedzai/openml/mocks/MockInstance.java
@@ -9,4 +9,18 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or impl
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.mocks;
+
+import com.feedzai.openml.data.Instance;
+import com.feedzai.openml.data.schema.AbstractValueSchema;
+import com.feedzai.openml.data.schema.DatasetSchema;
+import com.feedzai.openml.data.schema.FieldSchema;
+import com.feedzai.openml.data.schema.NumericValueSchema;
+import com.feedzai.openml.data.schema.StringValueSchema;
+import com.feedzai.openml.util.data.ClassificationDatas
