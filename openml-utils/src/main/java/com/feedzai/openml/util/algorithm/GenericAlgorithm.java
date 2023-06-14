f07ed76f805a4df41d88f5026857040d4d8c7aa4
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-06-14 17:59:34 +0000

    added message

--- openml-utils/src/main/java/com/feedzai/openml/util/algorithm/GenericAlgorithm.java
+++ openml-utils/src/main/java/com/feedzai/openml/util/algorithm/GenericAlgorithm.java
@@ -7,4 +7,17 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unl
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.feedzai.openml.util.algorithm;
+
+import com.feedzai.openml.provider.descriptor.MLAlgorithmDescriptor;
+import com.feedzai.openml.provider.descriptor.MachineLearningAlgorithmType;
+import com.google.common.collect.ImmutableSet;
+
+impo
