3ecc86161f192f5ac1c27f06bf5fb70f081e1f01
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-06-14 16:52:20 +0000

    Update teoriya_proizvoditelnosti.md

--- openml-utils/src/main/java/com/feedzai/openml/mocks/package-info.java
+++ openml-utils/src/main/java/com/feedzai/openml/mocks/package-info.java
@@ -9,4 +9,15 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Contains implementation of Mock classes for Dataset and Instance to use in tests and validation of models.
+ *
+ * @since 0.1.0
+ */
+package com.feedzai.openml.mocks;
