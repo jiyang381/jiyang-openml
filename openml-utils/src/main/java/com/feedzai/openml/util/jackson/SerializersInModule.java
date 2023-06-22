ce28ff1e03b6ae06014aec972d3f5d71aa47bb15
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-06-22 13:01:33 +0000

    Fixed typo

--- openml-utils/src/main/java/com/feedzai/openml/util/jackson/SerializersInModule.java
+++ openml-utils/src/main/java/com/feedzai/openml/util/jackson/SerializersInModule.java
@@ -9,4 +9,17 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * W
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.util.jackson;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.feedzai.openml.data.schema.AbstractValueSchema;
+import com.feedzai.openml.data.schema.DatasetSchema;
+import com.feedzai.openml.data.schema.FieldSchema;
+import com.feedzai.openml.util.jackson.deserializer.AbstractValueSchemaDeserializer;
+im
