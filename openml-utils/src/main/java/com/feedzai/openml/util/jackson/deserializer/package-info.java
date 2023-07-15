ed9e80e0d161bb746eed0adc0d4be3ccba422b44
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-07-15 12:15:59 +0000

    fix image path

--- openml-utils/src/main/java/com/feedzai/openml/util/jackson/deserializer/package-info.java
+++ openml-utils/src/main/java/com/feedzai/openml/util/jackson/deserializer/package-info.java
@@ -11,4 +11,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the Licens
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Contains the custom {@link com.fasterxml.jackson.databind.JsonDeserializer} used to serialize objects.
+ *
+ * @since 0.1.0
+ */
+package com.feedzai.openml.util.jackson.deserializer;
