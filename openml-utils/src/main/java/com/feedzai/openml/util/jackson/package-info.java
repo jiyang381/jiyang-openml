8e19e18ca46d6e7eb97052e7c76a2bc939767b8f
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-07-16 05:31:53 +0000

    Ignored files

--- openml-utils/src/main/java/com/feedzai/openml/util/jackson/package-info.java
+++ openml-utils/src/main/java/com/feedzai/openml/util/jackson/package-info.java
@@ -8,4 +8,17 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the Licens
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Contains the custom {@link com.fasterxml.jackson.databind.JsonDeserializer} and
+ * {@link com.fasterxml.jackson.databind.JsonSerializer} to allow the serialization of objects.
+ *
+ * @since 0.1.0
+ */
+package com.feedzai.openml.util.jackson;
