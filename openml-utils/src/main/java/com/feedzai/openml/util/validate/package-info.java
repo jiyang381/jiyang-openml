861195065d008c0aa52f6204cb50c9720fa1606d
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-08-24 17:02:36 +0000

    USB: serial: fix interface refcounting

--- openml-utils/src/main/java/com/feedzai/openml/util/validate/package-info.java
+++ openml-utils/src/main/java/com/feedzai/openml/util/validate/package-info.java
@@ -8,4 +8,16 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Contains helpers useful when validating the load and train of models.
+ *
+ * @since 0.1.0
+ */
+package com.feedzai.openml.util.validate;
