04137662fc7e869942942eea73d19035359e452f
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-09-06 14:16:55 +0000

    rename title

--- openml-utils/src/test/java/com/feedzai/openml/mocks/MockInstanceTest.java
+++ openml-utils/src/test/java/com/feedzai/openml/mocks/MockInstanceTest.java
@@ -10,4 +10,26 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permi
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.mocks;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Class that tests the behaviour 
