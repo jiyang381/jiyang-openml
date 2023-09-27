e12f76f27db8eb26c70cc16d320323cb792a9724
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-09-27 10:19:32 +0000

    added npm ignore

--- openml-utils/src/test/java/com/feedzai/openml/util/load/LoadModelUtilsTest.java
+++ openml-utils/src/test/java/com/feedzai/openml/util/load/LoadModelUtilsTest.java
@@ -8,4 +8,23 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on a
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.feedzai.openml.util.load;
+
+import com.feedzai.openml.provider.exception.ModelLoadingException;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for th
