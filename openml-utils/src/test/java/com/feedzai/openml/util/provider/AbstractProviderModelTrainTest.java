3784e014f1e99d0e3723c16a269ebf874faede64
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2023-10-26 19:41:58 +0000

    Fix method signature

--- openml-utils/src/test/java/com/feedzai/openml/util/provider/AbstractProviderModelTrainTest.java
+++ openml-utils/src/test/java/com/feedzai/openml/util/provider/AbstractProviderModelTrainTest.java
@@ -9,4 +9,18 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.feedzai.openml.util.provider;
+
+import com.feedzai.openml.data.Dataset;
+import com.feedzai.openml.model.ClassificationMLModel;
+import com.feedzai.openml.provider.MachineLearningProvider;
+import com.feedzai.openml.provider.exception.ModelTrainingException;
+import com.feedzai.openml.provider.model.MachineLearningModelTrainer;
+import com.feedzai.openml.util.algorithm.MLAlgorithmEnum;
+import org.junit.Test;
+
