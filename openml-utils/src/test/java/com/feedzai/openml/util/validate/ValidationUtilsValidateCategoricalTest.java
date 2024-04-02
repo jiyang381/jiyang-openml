f21afdad26b75515e524225933d395bc92a4ad47
jiyang381 <124852909+jiyang381@users.noreply.github.com>
2024-04-02 06:51:13 +0000

    Cleaned repo

--- openml-utils/src/test/java/com/feedzai/openml/util/validate/ValidationUtilsValidateCategoricalTest.java
+++ openml-utils/src/test/java/com/feedzai/openml/util/validate/ValidationUtilsValidateCategoricalTest.java
@@ -144,4 +144,8 @@ public class ValidationUtilsValidateCategoricalTest {
                         .withCategoricalFields(3, categories)
                         .withCategoricalTarget(useCategorical)
                         .build()
-  
+        );
+    }
+
+
+}
